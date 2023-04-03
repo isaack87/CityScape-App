4f5f75300d9f81104abe5130f8b031da9b3f6943
isaac <isaac.kim@electrameccanica.com>
2023-04-03 13:59:30 -0700

    Initial Commit

---
+++ CityScape App/CityScape_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityScape_AppApp.swift
+//  CityScape App
+//
+//  Created by Isaac Kim on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CityScape_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
